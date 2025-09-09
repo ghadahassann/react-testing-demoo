7426d9994ce1a0f6fadec6094abcbc970acde72b
ghada hassan <107355308+ghadahassann@users.noreply.github.com>
2025-09-09 09:32:41 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
     moduleFileExtensions: ['js','jsx','json'],
     transform: { '^.+\\.[tj]sx?$': 'babel-jest' }
+    collectCoverage: true,                                  // make Jest produce coverage
+    coverageDirectory: 'coverage',                          // write reports into ./coverage
+    coverageReporters: ['json', 'html', 'text-summary', 'lcov'] // 'json' is required for Codecov
+
 };
+
